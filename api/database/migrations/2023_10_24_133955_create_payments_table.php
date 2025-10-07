c6a3e9eaec6b02f8e149dd37e0e035584a8b810f
Adriano Andrade <webmail.adriano@gmail.com>
2025-10-07 16:31:20 -0300

    feat: api - add contracts and payments migrations

---
+++ api/database/migrations/2023_10_24_133955_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('contract_id');
+            $table->decimal('amount', 13, 2);
+            $table->date('due_date');
+            $table->date('payment_date')->nullable();
+            $table->boolean('paid')->default(false);
+            $table->timestamps();
+
+            $table->foreign('contract_id')->references('id')->on('contracts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
